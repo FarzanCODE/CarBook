ddfb4324164857496a17583af7b0a1f0c6e3a74c
FarzanCODE <farzan.farooquee@gmail.com>
2026-04-06 18:04:46 +0530

    feat: implement JWT-based authentication middleware and configure axios instance for API requests

--- carbook-frontend/src/api/axios.js
+++ carbook-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://carbook-1prw.onrender.com/api",
   withCredentials: true,
 });
 
